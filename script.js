b34b1026cac2cdfff5ef4c5fc10d3e4f6b08aa46
Alexey-Zanevsky <code0case@gmail.com>
2025-03-11 14:17:25 +0100

    5. Modified code

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
 'use strict'
 
-const menu = document.querySelector(".menu");
-menu.style.display = "none";
-const loadNumber = document.querySelector(".load-style");
-loadNumber.style.display = "none";
+// function sleep(ms) {
+//     return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
+document.querySelector(".start-button").addEventListener("click", async function () {
+    gsap.to(".snake-title", {
+        opacity: 0,
+        scale: .8,
+        duration: .5,
+        ease: "power1.out",
+        onComplete: function() {
+            document.querySelector(".snake-title").style.display = "none"
+        }
+    })
+    // await sleep(500); 
+    gsap.to(".rating", {
+        opacity: 0,
+        scale: .8,
+        duration: .5,
+        ease: "power1.out",
+        onComplete: function() {
+            document.querySelector(".rating").style.display = "none";
+        }
+    })
+    // await sleep(500); 
+    gsap.to(".flex-container", {
+        width: "100%", 
+        height: "100%", 
+        duration: .8,
+        ease: "power2.out",
+        onComplete: function() {
+            document.querySelector(".flex-container").style.justifyContent = "center";
+            document.querySelector(".flex-container").style.alignItems = "center";
+            document.querySelector(".snake-field").style.height = "90%";
+        }
+    })
+    gsap.to(".menu", {
+        opacity: 0,
+        scale: .8,
+        duration: .8,
+        ease: "power3.out",
+        onComplete: function() {
+            document.querySelector(".menu").style.display = "none";
+        }
+    })
+});
