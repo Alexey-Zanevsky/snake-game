0072fa6ef76b3a4be390392636c265ecadca8cc4
Alexey-Zanevsky <code0case@gmail.com>
2025-06-01 23:34:38 +0200

    last version

--- jscode/server.js
+++ jscode/server.js
@@ -5,6 +5,7 @@ const authRouter = require('./authRouter');
 // const PORT = process.env.PORT || 5000;
 const PORT = 5000;
 const config = require('./config');
+const path = require('path');
 
 const app = express();
 
